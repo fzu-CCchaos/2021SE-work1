219388b1456ecc70cc11d1ecf8ac7d94942b3e7e
fzu-CCchaos <1572280410@qq.com>
2021-09-16 10:47:58 +0800

    the combination of func1 and func2

---
+++ func1&2.py
@@ -0,0 +1,58 @@
+import string
+file_name=input()
+complete_level=input()
+def get_total_num(filename):
+    file = open(filename, "r", encoding='utf-8')
+    article = file.read()
+    article = article.replace(",","").replace(".","").replace("{"," ").replace("}"," ") \
+        .replace(":","").replace(";","").replace("?","").replace("("," ").replace(")"," ")
+    #exchange = article.lower()#转成小写字母
+    count = article.split()#生成单词列表
+    exclude = ["auto","break","case","char","const","continue","default","do","double","else","enum","extern",
+               "float","for","goto","if","int","long","register","return","short","signed","sizeof","static",
+               "struct","switch","typedef","union","unsigned","void","volatile","while"]#关键词列表
+    dic={}
+    for word in count:
+        if len(word)<2:#关键字没有单个字符的，可以排除
+            continue
+        else:
+            dic[word]=dic.get(word,0)+1#如果字典里键为word的值存在，则返回键的值并加一，不存在，则返回0再加1
+    for key in list(dic.keys()): #遍历字典的所有键，即所有word
+        if key not in exclude:
+            del dic[key]
+    total_num=0
+    for value in list(dic.values()):
+        total_num+=value
+    return [total_num,dic["switch"]]
+
+def get_case_num(filename):
+    case_num=[]
+    case_num_list=[]
+    with open(filename,'r') as f:
+        for line in f:
+            line1=line.strip().split('\n')
+            line2=''.join(line1)#将list转为string
+            if line2.find("switch")!=-1:
+                case_num.append('1')
+            elif line2.find("case")!=-1:
+                case_num.append('2')
+    i=0
+    num_of_case=0
+    for i in range(len(case_num)):
+        if case_num[i]=='1' and num_of_case!=0:
+            case_num_list.append(num_of_case)
+            num_of_case=0
+        elif case_num[i]=='2':
+            num_of_case+=1
+            if i==len(case_num)-1:
+                case_num_list.append(num_of_case)
+    return case_num_list
+
+if complete_level=='1':
+    print("total num: ",get_total_num(file_name)[0])
+elif complete_level=='2':
+    print("total num: ",get_total_num(file_name)[0])
+    print("switch num: ",get_total_num(file_name)[1])
+    print("case num:",end=" ")
+    for i in get_case_num(file_name):
+        print(i,end=' ')
